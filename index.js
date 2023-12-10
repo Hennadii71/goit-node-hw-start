d065b7e52f71fcb2319e774c9de445bcc5a4cdae
Hennadii71 <genadisema@gmail.com>
2023-12-10 21:53:17 +0200

    Update index.js and contacts.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+const argv = require("yargs").argv;
 
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await listContacts();
+      return console.table(allContacts);
+      break;
+    case "getId":
+      const contactId = await getContactById(id);
+      return console.log(contactId);
+      break;
+    case "remove":
+      const deleteContact = await removeContact(id);
+      console.log(deleteContact);
+      break;
+    case "add":
+      const newContact = await addContact(name, email, phone);
+      console.log(newContact);
+      break;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
